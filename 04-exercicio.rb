c3d85e84117ef5a2c601341885b07071158d5104
Leandro Ikehara <leandro.ikehara@gmail.com>
2021-07-13 10:00:54 -0300

    Correção dos exercício 04

--- 04-exercicio.rb
+++ 04-exercicio.rb
@@ -4,18 +4,34 @@
 
 # 1) O diâmetro de um círculo é 2x o seu raio.
 def diametro(raio)
-    return raio = raio * 2
+    return raio * 2
 end
 
-raio = 5
+raio = 10
 puts "Diâmetro do círculo: " + diametro(raio).to_s
+
+def calcula_diametro(raio)   # Resolução Ivo
+    return raio * 2          #
+end                          #
+                             # 
+raio = 10                    #
+puts calcula_diametro(raio)  #
+
 
 # 2) O comprimento de um círculo é seu diâmetro vezes o valor da constante matemática PI (3.1415...)
 def comprimento(diametro, pi)
     return diametro * pi
 end
 
-puts "Comprimento do círculo: " + comprimento(10, Math::PI).to_s
+puts "Comprimento do círculo: " + comprimento(diametro(raio), Math::PI).to_s
+
+
+def calcula_comprimento(raio)                 # Resolução Ivo
+    return calcula_diametro(raio) * Math::PI  #
+end                                           #
+                                              #
+raio = 10                                     #
+puts calcula_comprimento(raio)                #
 
 # 3) A área de um círculo é seu raio ao quadrado vezes o valor da constante matemática PI (3.1415...)
 def circulo(raio, pi)
@@ -23,6 +39,24 @@ def circulo(raio, pi)
 end
 
 puts "Área do círculo: " + circulo(raio, Math::PI).to_s
+
+def calcula_area(raio)           # Resolução Ivo
+    return raio ** 2 * Math::PI  #
+end                              #
+                                 #
+raio = 10                        #
+puts calcula_area(raio)          #
+
+
+raio = 5
+puts diametro(raio)
+puts comprimento(diametro(raio), Math::PI)
+puts circulo(raio, Math::PI)
+
+raio = 5
+puts calcula_diametro(raio)       # Resolução Ivo
+puts calcula_comprimento(raio)    #
+puts calcula_area(raio)           #
 
 #Escreva funcões para calcular o diâmetro, o comprimento e a área de um círculo com base no valor do seu raio.
 #Ou seja, escreva uma função para cada cálculo, considerando que o raio será informado na chamada de cada função.
