776d9c37b8ccf6c1d90e01ce249c81078309d538
Leandro Ikehara <leandro.ikehara@gmail.com>
2021-07-21 17:53:54 -0300

    Finalização dos Exercícios do Simulado

--- simulado/05-simulado.rb
+++ simulado/05-simulado.rb
@@ -1,4 +1,4 @@
-# SIMULADO - fazendo...
+# SIMULADO
 
 =begin 
 5) Faça uma função chamada 'mediana' que recebe um array de números e retorna a mediana entre eles.
@@ -7,15 +7,15 @@ Lembrando que para obter a mediana de uma série de números,
 - se a série tiver um número (n) ímpar de elementos, retornar o número da posição n/2 (a parte inteira);
 - e caso se a série tiver um número (n) par de elementos, retornar a média entre os números das posições (n/2)-1 e (n/2)
 =end
+
+n = 0
 def mediana(array)
     array = array.sort()
-    for n in (0..array.size-1)
-        (array.size-1)/2
-        if n = n/2
-            return mediana
-        else
-        return n = (n/2)-1
-        end
+    n = array.size
+    if n % 2 != 0
+        return array[n/2]
+    elsif n % 2 == 0
+        return array[n/2-1] + array[n/2]/2
     end
 end
 
@@ -23,3 +23,6 @@ end
 #Ex.:
 puts(mediana([10, 0, -1, -500, 20, 100])) # deve imprimir 5.0
 puts(mediana([10, 0, -1, -500, 20]))      # deve imprimir 0.0 
+
+puts(mediana([-10, 5, 8, 12, 25]))
+puts(mediana([-7, 20, 15, 13, -5, 40]))
--- simulado/06-simulado.rb
+++ simulado/06-simulado.rb
@@ -1,4 +1,4 @@
-# SIMULADO - fazendo...
+# SIMULADO
 
 
 =begin
@@ -6,6 +6,28 @@
 contendo apenas 2 elementos: o primeiro deve ser o menor número do array informado e o segundo deve ser o maior.
 =end
 
+def menor_maior(array)
+    for n in (0..array.size-1)
+        if n == 0
+        maior = array[n]
+        menor = array[n]
+        else
+            if array[n] > maior
+                maior = array[n]
+            elsif array[n] < menor
+                menor = array[n]
+            end            
+        end
+    end
+    return [menor, maior]
+end
+
 #Ex.:
 print(menor_maior([10, 0, -1, -500, 20, 100])) # deve imprimir [-500, 100]
 puts 
+
+print(menor_maior([40, 8, 40, 80, 30, 10]))
+puts
+
+print(menor_maior([-10, 40, 0, 80, 110]))
+puts
