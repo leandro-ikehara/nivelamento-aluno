ab703faf62860bb512eabc8d22b9d16f7d698fe3
Leandro Ikehara <leandro.ikehara@gmail.com>
2021-07-23 10:59:41 -0300

    Ajustes exercicio 02 da avaliação

--- avaliacao/02-avaliacao.rb
+++ avaliacao/02-avaliacao.rb
@@ -6,18 +6,27 @@ e deve retornar um valor numérico que deve ser o resultado da soma da multiplic
 os elementos que ocupam a mesma posição em cada array.
 =end
 
-
 def multiplica_arrays(array1, array2)
-    array1.size == array2.size
-    for n in (0..array1.size-1) && (0..array2.size-2)
-        if array1[n] = array2[n]
-            multiplica = array1[n] * array2[n]
-            n = n + 1
-        return multiplica
+    if array1.size == array2.size
+        multiplica = []
+        for n in (0..array1.size-1)
+            multiplica[n] = array1[n] * array2[n]
         end 
+        soma = 0           
+        for n in (0..multiplica.size-1)
+            soma = soma + multiplica[n]
+        end
+        return soma
+    else
+        return nil
     end
 end
 
+# Testes:
+puts multiplica_arrays([1, 2, 3], [10, 9, 8]) 
+puts multiplica_arrays([2, 3], [10, 20, 30]).to_s + "nil"
+puts multiplica_arrays([2, 4, 6, 8], [2, 4, 6, 8])
+puts
 
 # Ex.:
 puts multiplica_arrays([1, 2, 3], [10, 20, 30]) # deve imprimir 140
